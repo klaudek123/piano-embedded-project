73716327be1d519ef6b145c3b7cc194aaf13bde8
klaudek123 <klaudiusz1tai@gmail.com>
2023-12-20 15:40:22 +0100

    add features

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
+import time
 import tkinter as tk
 
 import numpy as np
 import pygame
 import sounddevice as sd
 
+import sqlite3
+
+# Tworzymy lub łączymy się z bazą danych
+
+
+# Tworzymy tabelę w bazie danych
+
+
+# Funkcja zapisująca sekwencję do bazy danych SQLite
+def zapisz_do_bazy(klawisz_lista, czas_lista):
+    print("dupa")
+    print(klawisz_lista)
+    print(czas_lista)
+    conn = sqlite3.connect('sekwencje_pianina.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Sekwencje (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    klawisz TEXT,
+                    czas_nacisniecia REAL)''')
+    for i in range(len(klawisz_lista)):
+        cursor.execute('''INSERT INTO Sekwencje (klawisz, czas_nacisniecia)
+                        VALUES (?, ?)''', (klawisz_lista[i], czas_lista[i]))
+    conn.commit()
+    for row in cursor.execute("SELECT * FROM Sekwencje"):
+        print(row)
+
+  
+
+# Częstotliwości dźwięków klawiszy
 frequencies = {
     'C4': 261.63,
     'C#4': 277.18,
@@ -31,6 +61,9 @@ frequencies = {
     'H5': 987.77
 }
 
+# Lista klawiszy pianina
+klawisze_pianina = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'H4', 'C5', 'C#5', 'D5',
+                    'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'H5']
 
 # Tutaj funkcja odtwarzająca dźwięk dla danego klawisza
 def zagraj_dzwiek(klawisz):
@@ -47,6 +80,47 @@ def zagraj_dzwiek(klawisz):
 def klikniecie_klawisza(klawisz):
     zagraj_dzwiek(klawisz)
 
+    if nagrywanie == True:
+        czas_lista.append(time.time() - czas_nacisniecia)
+        klawisz_lista.append(klawisz)
+
+
+nagrywanie = False
+
+czas_lista = []
+klawisz_lista = []
+czas_nacisniecia = time.time()
+
+
+# def nagrywaj_sekwencje():
+#     def zapisz_sekwencje():
+#
+#         nonlocal nagrywanie, czas_start, sekwencja
+#         if not nagrywanie:
+#             return
+#
+#         for idx, klawisz in enumerate(klawisze_pianina):
+#             if pygame.key.get_pressed()[pygame.K_SPACE]:  # Przykładowy klawisz do zatrzymania nagrywania (spacja)
+#                 nagrywanie = False
+#                 break  # Przerwij pętlę gdy nagrywanie zostanie zatrzymane
+#
+#             if pygame.key.get_pressed()[idx]:
+#                 czas_naciśnięcia = time.time() - czas_start
+#
+#                 sekwencja.append((klawisz, czas_naciśnięcia))
+#
+#         if czas_naciśnięcia >= 20:  # Przykładowy czas nagrywania [s]
+#             nagrywanie = False
+#
+#         if nagrywanie:
+#             root.after(10, zapisz_sekwencje)  # Kontynuuj nagrywanie co 10 ms
+#
+#
+#     nagrywanie = True
+#     czas_start = time.time()
+#     sekwencja = []
+#     zapisz_sekwencje()  # Rozpocznij nagrywanie
+
 
 # Inicjalizacja Pygame (do obsługi dźwięków)
 pygame.init()
@@ -55,21 +129,35 @@ pygame.init()
 root = tk.Tk()
 root.title("Wirtualne pianino")
 root.geometry("1280x400")
-# Lista klawiszy pianina
-klawisze_pianina = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'H4', 'C5', 'C#5', 'D5',
-                    'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'H5']
 
+def nagrywanie_fun():
+    global nagrywanie
+    global klawisz_lista
+    global czas_lista
+    nagrywanie = not nagrywanie
+
+    if nagrywanie == False:
+        zapisz_do_bazy(klawisz_lista,czas_lista)
+
+        klawisz_lista = []
+        czas_lista = []
+
+
+def stworz_przycisk_nagrywania():
+    return tk.Button(root, text="Nagrywaj sekwencję", command=lambda: nagrywanie_fun())
+
+
+przycisk_nagrywania = stworz_przycisk_nagrywania()
+przycisk_nagrywania.pack()
 
 # Funkcja tworząca przyciski dla klawiszy pianina
 def stworz_klawisz(klawisz):
     return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz,
                      command=lambda: klikniecie_klawisza(klawisz))
 
-
 def stworz_klawisz_czarny(klawisz):
     return tk.Button(root, width=3, height=5, bg='black', fg='white', font=("arial", 12, "bold"), text=klawisz,
                      command=lambda: klikniecie_klawisza(klawisz))
-
 
 def stworz_klawisz(klawisz, czarny=False):
     if czarny:
@@ -79,10 +167,9 @@ def stworz_klawisz(klawisz, czarny=False):
         return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz,
                          command=lambda: klikniecie_klawisza(klawisz))
 
-
 # Dodanie klawiszy do interfejsu na zmianę
 for i in range(len(klawisze_pianina)):
-    if ('#' in klawisze_pianina[i]):
+    if '#' in klawisze_pianina[i]:
         przycisk_czarny = stworz_klawisz(klawisze_pianina[i], True)
         przycisk_czarny.pack(side=tk.LEFT)
     else:
