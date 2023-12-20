f4f2cec33a5db8688236c014ec6a50b78535814f
klaudek123 <klaudiusz1tai@gmail.com>
2023-12-20 13:12:41 +0100

    add base code

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import tkinter as tk
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Tutaj funkcja odtwarzająca dźwięk dla danego klawisza
+def zagraj_dzwiek(klawisz):
+    # Implementacja odtwarzania dźwięku dla danego klawisza
+    print(f"Odtwarzam dźwięk dla klawisza {klawisz}")
+
+# Funkcja obsługująca kliknięcie klawisza na interfejsie
+def klikniecie_klawisza(klawisz):
+    zagraj_dzwiek(klawisz)
+
+# Inicjalizacja Pygame (do obsługi dźwięków)
+pygame.init()
+
+# Tworzenie okna głównego
+root = tk.Tk()
+root.title("Wirtualne pianino")
+
+# Lista klawiszy pianina
+klawisze_pianina = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+# Funkcja tworząca przyciski dla klawiszy pianina
+def stworz_klawisz(klawisz):
+    return tk.Button(root, text=klawisz, command=lambda: klikniecie_klawisza(klawisz))
+
+# Dodanie klawiszy do interfejsu
+for klawisz in klawisze_pianina:
+    przycisk = stworz_klawisz(klawisz)
+    przycisk.pack(side=tk.LEFT)
+
+# Rozpoczęcie głównej pętli programu
+root.mainloop()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
