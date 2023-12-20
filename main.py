c5f0a8c18f1eaa348f74cafbeb5ee56ee300e6f6
klaudek123 <klaudiusz1tai@gmail.com>
2023-12-20 13:54:04 +0100

    change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-import pygame
+
 import numpy as np
+import pygame
 import sounddevice as sd
 
 frequencies = {
@@ -29,6 +30,8 @@ frequencies = {
     'A#5': 932.33,
     'H5': 987.77
 }
+
+
 # Tutaj funkcja odtwarzająca dźwięk dla danego klawisza
 def zagraj_dzwiek(klawisz):
     # Implementacja odtwarzania dźwięku dla danego klawisza
@@ -39,9 +42,11 @@ def zagraj_dzwiek(klawisz):
     # Odtworzenie dźwięku
     sd.play(note, sample_rate)
 
+
 # Funkcja obsługująca kliknięcie klawisza na interfejsie
 def klikniecie_klawisza(klawisz):
     zagraj_dzwiek(klawisz)
+
 
 # Inicjalizacja Pygame (do obsługi dźwięków)
 pygame.init()
@@ -51,31 +56,33 @@ root = tk.Tk()
 root.title("Wirtualne pianino")
 root.geometry("1280x400")
 # Lista klawiszy pianina
-klawisze_pianina = ['C4','C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'H4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'H5']
+klawisze_pianina = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'H4', 'C5', 'C#5', 'D5',
+                    'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'H5']
+
 
 # Funkcja tworząca przyciski dla klawiszy pianina
 def stworz_klawisz(klawisz):
-    return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz ,command=lambda: klikniecie_klawisza(klawisz))
+    return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz,
+                     command=lambda: klikniecie_klawisza(klawisz))
+
 
 def stworz_klawisz_czarny(klawisz):
-    return tk.Button(root, width=3, height=5, bg='black', fg='white', font=("arial", 12, "bold"), text=klawisz, command=lambda: klikniecie_klawisza(klawisz))
+    return tk.Button(root, width=3, height=5, bg='black', fg='white', font=("arial", 12, "bold"), text=klawisz,
+                     command=lambda: klikniecie_klawisza(klawisz))
+
 
 def stworz_klawisz(klawisz, czarny=False):
     if czarny:
-        return tk.Button(root, width=3, height=5, bg='black', fg='white', font=("arial", 12, "bold"), text=klawisz, command=lambda: klikniecie_klawisza(klawisz))
+        return tk.Button(root, width=3, height=5, bg='black', fg='white', font=("arial", 12, "bold"), text=klawisz,
+                         command=lambda: klikniecie_klawisza(klawisz))
     else:
-        return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz, command=lambda: klikniecie_klawisza(klawisz))
+        return tk.Button(root, width=4, height=8, bg='white', fg='grey', font=("arial", 18, "bold"), text=klawisz,
+                         command=lambda: klikniecie_klawisza(klawisz))
+
 
 # Dodanie klawiszy do interfejsu na zmianę
 for i in range(len(klawisze_pianina)):
-    #przycisk_bialy = stworz_klawisz(klawisze_pianina[i])
-    # przycisk_bialy.pack(side=tk.LEFT)
-    #
-    # # Sprawdzenie, czy są jeszcze klawisze czarne do dodania
-    # if i < len(klawisze_czarne):
-    #     przycisk_czarny = stworz_klawisz(klawisze_czarne[i], czarny=True)
-    #     przycisk_czarny.pack(side=tk.LEFT, padx=(0, 10))  # Dodanie odstępu między klawiszami czarnymi
-    if( '#' in klawisze_pianina[i]):
+    if ('#' in klawisze_pianina[i]):
         przycisk_czarny = stworz_klawisz(klawisze_pianina[i], True)
         przycisk_czarny.pack(side=tk.LEFT)
     else:
@@ -84,5 +91,3 @@ for i in range(len(klawisze_pianina)):
 
 # Rozpoczęcie głównej pętli programu
 root.mainloop()
-
-
